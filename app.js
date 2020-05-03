8d19c14fc9747df8c08514432cc9340ad099f0e2
RoiEf <roi.efrati@gmail.com>
2020-05-04 02:12:47 +0300

    First Commit. Node.JS Express base.

---
+++ app.js
@@ -0,0 +1,57 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+
+// const aboutRoutes = require("./api/about");
+// const servicesRoutes = require("./api/services");
+// const iotApi = require("./api/IoT_API");
+// const PublicRoutes = require("./api/public");
+
+/**
+ *  App Configuration
+ */
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// start monitoring
+app.use(morgan("dev"));
+// handling cors errors
+app.use(cors());
+// parsing request data body
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+  res.status(200).send("Base IoT Mock API");
+});
+// app.use("/about", aboutRoutes);
+// app.use("/services", servicesRoutes);
+// app.use("/iotApi", iotApi);
+// app.use("/public", PublicRoutes);
+
+// error handling
+// 404 Not found page errors
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+// chatch all other system errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+  });
+});
+
+module.exports = app;
