35ac4af76b7fc1dcd23ca259cfa312db443f6349
RoiEf <roi.efrati@gmail.com>
2020-05-05 22:45:19 +0300

    Added content to the login CTLR

--- controllers/loginCTL.js
+++ controllers/loginCTL.js
@@ -1,5 +1,12 @@
 exports.login = (req, res) => {
+  //   console.log("userName: ", req.body);
+  if (req.body.userName === "admin" && req.body.password === "12345") {
     return res.status(200).json({
-        message: "Auth sucess"
+      message: "Auth sucess",
     });
+  } else {
+    return res.status(401).json({
+      message: "Auth Faild",
+    });
+  }
 };
