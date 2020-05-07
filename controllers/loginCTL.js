36202bb532d8db461fb5997cbf66546a5cee2984
RoiEf <roi.efrati@gmail.com>
2020-05-07 23:08:36 +0300

    1. Added node-persist 2. embedded node-persist into loginCTL

--- controllers/loginCTL.js
+++ controllers/loginCTL.js
@@ -1,11 +1,20 @@
-exports.login = (req, res) => {
+const storage = require("node-persist");
+
+exports.login = async (req, res) => {
+  await storage.init(/* options ... */);
+  // await storage.setItem("password", "12345");
+  let userName = await storage.getItem("userName");
+  let password = await storage.getItem("password");
+  // console.log("userName: ", userName);
+  // console.log("password: ", password);
+
   //   console.log("userName: ", req.body);
-  if (req.body.userName === "admin" && req.body.password === "12345") {
+  if (req.body.userName === userName && req.body.password === password) {
     return res.status(200).json({
       message: "Auth sucess",
     });
   } else {
-    return res.status(401).json({
+    return res.status(200).json({
       message: "Auth Faild",
     });
   }
