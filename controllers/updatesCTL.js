77f4879509f3c1a47bdfb6becd8de8eb48a90737
Roi <roi.efrati@gmail.com>
2020-09-08 18:39:27 +0300

    Removed consul.log debug calls

--- controllers/updatesCTL.js
+++ controllers/updatesCTL.js
@@ -16,16 +16,16 @@ exports.password = async (req, res) => {
   await storage.init(/* options ... */);
   let userName = await storage.getItem("userName");
   let password = await storage.getItem("password");
-  console.log("userName: ", userName);
-  console.log("password: ", password);
-  console.log("req.fields.userName: ", req.fields.userName);
-  console.log("req.fields.password: ", req.fields.password);
-  console.log("req.fields.password1: ", req.fields.password1);
-  console.log("req.fields.password2: ", req.fields.password2);
+  // console.log("userName: ", userName);
+  // console.log("password: ", password);
+  // console.log("req.fields.userName: ", req.fields.userName);
+  // console.log("req.fields.password: ", req.fields.password);
+  // console.log("req.fields.password1: ", req.fields.password1);
+  // console.log("req.fields.password2: ", req.fields.password2);
 
   if (req.fields.userName === userName && req.fields.password === password) {
     if (req.fields.password1 === req.fields.password2 && req.fields.password1.length > 4) {
-      console.log("password1: ", req.fields.password1);
+      // console.log("password1: ", req.fields.password1);
       storage.setItem("password", req.fields.password1);
       return res.status(200).json({
         message: "Password Update sucess",
