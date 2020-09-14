fd8dd4d28a596a2363ba5388e234a10d2611f187
Roi <roi.efrati@gmail.com>
2020-09-14 16:24:45 +0300

    Wifi basic settings refactoring device mode done

--- controllers/networkCTL.js
+++ controllers/networkCTL.js
@@ -4,6 +4,7 @@ exports.network = async (req, res) => {
   await storage.init(/* options ... */);
   // START await storage.setItem(); INIT ITEMS RUN ONLY ONCE
   {
+    // await storage.setItem("device_mode", "AP");
     // await storage.setItem("ssid", "base_iot");
     // await storage.setItem("wifiPassword", "");
     // await storage.setItem("dhcpMode", "DHCP");
@@ -22,17 +23,20 @@ exports.network = async (req, res) => {
   }
   // END await storage.setItem(); INIT ITEMS RUN ONLY ONCE
 
+  let device_mode;
   let ssid;
   let wifiPassword;
   let dhcp;
   let ip1, ip2, ip3, ip4, sm1, sm2, sm3, sm4, dg1, dg2, dg3, dg4;
-
+  let message;
 
   if (req.fields.cmd === "update") {
     console.log("wifi updating");
     console.log("req.body: ", req.fields);
     storage.setItem("ssid", req.fields.ssid);
     storage.setItem("wifiPassword", req.fields.wifiPassword);
+
+    device_mode = await storage.getItem("device_mode");
     dhcp = await storage.getItem("dhcpMode");
     ssid = req.fields.ssid;
     wifiPassword = req.fields.wifiPassword;
@@ -48,6 +52,7 @@ exports.network = async (req, res) => {
       dhcp = "STATIC";
     }
 
+    device_mode = await storage.getItem("device_mode");
     ssid = await storage.getItem("ssid");
     wifiPassword = await storage.getItem("wifiPassword");
   } else if (req.fields.cmd === "updateStaticIP") {
@@ -79,10 +84,37 @@ exports.network = async (req, res) => {
     storage.setItem("dg3", dg3);
     storage.setItem("dg4", dg4);
 
+    device_mode = await storage.getItem("device_mode");
     ssid = await storage.getItem("ssid");
     wifiPassword = await storage.getItem("wifiPassword");
     dhcp = await storage.getItem("dhcpMode");
+  } else if (req.fields.cmd === "updateDeviceMode") {
+    if (req.fields.wifiAP) {
+      storage.setItem("device_mode", "AP");
+      device_mode = "AP";
+    } else {
+      storage.setItem("device_mode", "STA");
+      device_mode = "STA";
+    }
+
+    message = "Device Mode Update sucess";
+    ssid = await storage.getItem("ssid");
+    wifiPassword = await storage.getItem("wifiPassword");
+    dhcp = await storage.getItem("dhcpMode");
+    ip1 = await storage.getItem("ip1");
+    ip2 = await storage.getItem("ip2");
+    ip3 = await storage.getItem("ip3");
+    ip4 = await storage.getItem("ip4");
+    sm1 = await storage.getItem("sm1");
+    sm2 = await storage.getItem("sm2");
+    sm3 = await storage.getItem("sm3");
+    sm4 = await storage.getItem("sm4");
+    dg1 = await storage.getItem("dg1");
+    dg2 = await storage.getItem("dg2");
+    dg3 = await storage.getItem("dg3");
+    dg4 = await storage.getItem("dg4");
   } else {
+    device_mode = await storage.getItem("device_mode");
     ssid = await storage.getItem("ssid");
     wifiPassword = await storage.getItem("wifiPassword");
     dhcp = await storage.getItem("dhcpMode");
@@ -100,6 +132,7 @@ exports.network = async (req, res) => {
     dg4 = await storage.getItem("dg4");
   }
 
+  console.log("device_mode: ", device_mode);
   console.log("ssid: ", ssid);
   console.log("wifiPassword: ", wifiPassword);
   console.log("dhcpMode: ", dhcp);
@@ -107,6 +140,8 @@ exports.network = async (req, res) => {
   console.log("sm1: ", sm1);
   console.log("dg1: ", dg1);
   return res.status(200).json({
+    message,
+    device_mode,
     ssid,
     wifiPassword,
     dhcp,
